52c531913fc29999e5314fdab92d5ba8b07f381b
Niels <nvoogt@redcross.nl>
2026-06-15 13:19:03 +0200

    Addressing copilot comments

--- interfaces/portal/src/app/pages/program-settings-registration-data/components/kobo-integration-card/kobo-integration-card.component.ts
+++ interfaces/portal/src/app/pages/program-settings-registration-data/components/kobo-integration-card/kobo-integration-card.component.ts
@@ -21,13 +21,17 @@ import {
 } from '~/domains/kobo/kobo.helpers';
 import { KoboApiService } from '~/domains/kobo/kobo-api.service';
 import { KoboConfigurationDialogComponent } from '~/pages/program-settings-registration-data/components/kobo-configuration-dialog/kobo-configuration-dialog.component';
-import { KoboErrorDialogComponent } from '~/pages/program-settings-registration-data/components/kobo-error-dialog/kobo-error-dialog.component';
 import { KoboImportExistingRegistrationsDialogComponent } from '~/pages/program-settings-registration-data/components/kobo-import-existing-registrations-dialog/kobo-import-existing-registration-dialog.component';
 import { ToastService } from '~/services/toast.service';
 
 @Component({
   selector: 'app-kobo-integration-card',
-  imports: [CardWithLinkComponent, DatePipe, KoboConfigurationDialogComponent],
+  imports: [
+    CardWithLinkComponent,
+    DatePipe,
+    KoboConfigurationDialogComponent,
+    KoboImportExistingRegistrationsDialogComponent,
+  ],
   templateUrl: './kobo-integration-card.component.html',
   styles: ``,
   providers: [ToastService],
@@ -43,13 +47,11 @@ export class KoboIntegrationCardComponent {
     viewChild.required<KoboConfigurationDialogComponent>(
       'koboConfigurationDialog',
     );
+
   readonly koboImportExistingDialog =
     viewChild.required<KoboImportExistingRegistrationsDialogComponent>(
       'koboImportExistingDialog',
     );
-
-  readonly koboErrorDialog =
-    viewChild.required<KoboErrorDialogComponent>('koboErrorDialog');
 
   readonly koboIntegration = injectQuery(() => ({
     ...this.koboApiService.getKoboIntegration(this.programId)(),
